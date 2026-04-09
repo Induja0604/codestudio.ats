4ac0889b71e6a6614f23076935a348011e29b0ea
induja <indujanarnavaram@gmail.com>
2026-04-09 22:04:46 +0530

    backend change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://codestudio-ats.onrender.com/api"
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
